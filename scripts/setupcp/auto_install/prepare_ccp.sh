5d9ed28fbfea3a6621f49a1756a092f8cabb19a1
Landon Bouma <landon.bouma@gmail.com>
2016-07-18 12:06:34 -0500

    Debugging issue.

--- scripts/setupcp/auto_install/prepare_ccp.sh
+++ scripts/setupcp/auto_install/prepare_ccp.sh
@@ -17,6 +17,7 @@ set -e
 
 #script_relbase=$(dirname $0)
 #script_absbase=`pwd $script_relbase`
+echo SCRIPT_DIR=$(dirname $(readlink -f $0))
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 # SYNC_ME: This block of code is shared.
